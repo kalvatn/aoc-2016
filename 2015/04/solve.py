81cf90791ea01079e3485e43b6cf50ac1082c4d5
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2016-12-05 22:15:04 +0100

    2015 day 4 solution

--- 2015/04/solve.py
+++ 2015/04/solve.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
 
+import hashlib
+
 def main(lines):
-    print lines
+    secret = lines[0]
+    index = 0
+    print secret
+    while True:
+        digest = hashlib.md5(secret + str(index)).hexdigest()
+        if digest.startswith('00000'):
+            break
+        index += 1
+    print index
+
 
 
 if __name__ == '__main__':
