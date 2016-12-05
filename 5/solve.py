6b5591b328a77add4606da3a49ad0368fa96df5c
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2016-12-05 16:56:17 +0100

    solve day 5

--- 5/solve.py
+++ 5/solve.py
@@ -1,8 +1,32 @@
 #!/usr/bin/env python
 
-def main(lines):
-    print lines
+import hashlib
 
+def main(lines):
+    door_id = lines[0]
+    # door_id = 'abc'
+    password = ''
+
+    index = 0
+    while (len(password) < 8):
+        unhashed = door_id + str(index)
+        hashed = hashlib.md5(unhashed).hexdigest()
+
+        if is_password_hash(hashed):
+            print unhashed
+            print hashed
+            password += get_password_character(hashed)
+            print password
+        index += 1
+
+    print password
+
+
+def is_password_hash(hashed):
+    return hashed.startswith('00000')
+
+def get_password_character(hashed):
+    return hashed[5]
 
 if __name__ == '__main__':
     lines = []
