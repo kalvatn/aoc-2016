6a5deaef45a05bf62ba0dbd82770d59c3fac308a
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2016-12-05 14:01:30 +0100

    parse tokens

--- 4/solve.py
+++ 4/solve.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
 
 def main(lines):
-    print lines
+    for room in lines:
+        last_dash = room.rfind('-')
+        first_bracket = room.rfind('[')
+        last_bracket = room.rfind(']')
+        name = room[0:last_dash]
+        sector_id = room[last_dash+1:first_bracket]
+        checksum = room[first_bracket+1:last_bracket]
+        print room
+        print name
+        print sector_id
+        print checksum
+
 
 
 if __name__ == '__main__':
