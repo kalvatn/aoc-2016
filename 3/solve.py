761d17b976cdf41c5b5270b5d6228a0f16f7be3e
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2016-12-05 13:50:50 +0100

    solve day 3

--- 3/solve.py
+++ 3/solve.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 
 def main(lines):
-    print lines
+    possible = 0
+    for line in lines:
+        sides = [int(x) for x in line.strip().split()]
+        largest = sides.pop(sides.index(max(sides)))
+        sum_smaller = sum(sides)
+        if sum_smaller > largest:
+            possible += 1
+    print possible
+
+
+
+
+
 
 
 if __name__ == '__main__':
