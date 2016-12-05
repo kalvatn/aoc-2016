f2eb4048f7eec511d32c6a688e6e2e0d720653f8
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2016-12-05 21:21:08 +0100

    solve 2015 day 2

--- 2015/2/solve.py
+++ 2015/2/solve.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
 
 def main(lines):
-    print lines
+    total_paper = 0
+    for dimensions in lines:
+    # for dimensions in ['1x1x10', '2x3x4']:
+        length, width, height = [int(x) for x in dimensions.split('x')]
+        x = length * width
+        y = width * height
+        z = height * length
+
+        area = (2 * x + 2 * y + 2 * z)
+        smallest_side = min([x, y, z])
+        paper = area + smallest_side
+        total_paper += paper
+    print total_paper
+
 
 
 if __name__ == '__main__':
