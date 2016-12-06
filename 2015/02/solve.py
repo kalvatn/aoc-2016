7872d249aa0b1654e9205a22d10e9e2ab634ec07
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2016-12-06 18:50:55 +0100

    2015 day 2 part 2 solution

--- 2015/02/solve.py
+++ 2015/02/solve.py
@@ -2,6 +2,7 @@
 
 def main(lines):
     total_paper = 0
+    total_ribbon = 0
     for dimensions in lines:
     # for dimensions in ['1x1x10', '2x3x4']:
         length, width, height = [int(x) for x in dimensions.split('x')]
@@ -9,11 +10,19 @@ def main(lines):
         y = width * height
         z = height * length
 
+        volume = length * width * height
+        sides = [length, width, height]
+        sides.pop(sides.index(max(sides)))
+        smallest_perimiter = sum([ 2 * x for x in sides ])
+        total_ribbon += volume + smallest_perimiter
+
         area = (2 * x + 2 * y + 2 * z)
         smallest_side = min([x, y, z])
         paper = area + smallest_side
         total_paper += paper
-    print total_paper
+
+    print 'part 1 : %d' % (total_paper)
+    print 'part 2 : %d' % (total_ribbon)
 
 
 
