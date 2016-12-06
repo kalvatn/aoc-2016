af5563c33ed3d2d29d479aa3252a56045cf1e398
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2016-12-06 19:31:43 +0100

    2015 day 3 part 2 solution

--- 2015/03/solve.py
+++ 2015/03/solve.py
@@ -6,41 +6,78 @@ DOWN  = 'v'
 RIGHT = '>'
 LEFT  = '<'
 
-VISITED = {
+PART_ONE_VISITED = {
+    (0,0) : 1
+}
+SANTA_VISITED = {
+    (0,0) : 1
+}
+ROBOT_VISITED = {
     (0,0) : 1
 }
 
+ALL = 'all'
+SANTA = 'santa'
+ROBOT = 'robot'
+
 def main(lines):
-    # print VISITED
+    # lines = [
+    #     # '^v',
+    #     # '^>v<',
+    #     '^v^v^v^v^v'
+    # ]
     directions = lines[0]
 
-    pos_x = 0
-    pos_y = 0
+    pos = {
+        SANTA : [0, 0],
+        ROBOT : [0, 0],
+        ALL : [0,0]
+    }
 
+    turn = SANTA
     for direction in directions:
-    # for direction in '>':
-    # for direction in '^>v<':
-    # for direction in '^v^v^v^v^v':
-        if direction == UP:
-            pos_y += 1
-        if direction == DOWN:
-            pos_y -= 1
-        if direction == LEFT:
-            pos_x -= 1
-        if direction == RIGHT:
-            pos_x += 1
+        x_change = 0
+        y_change = 0
+        if direction in [ UP, DOWN ]:
+            y_change = 1 if direction == UP else -1
+        if direction in [ LEFT, RIGHT ]:
+            x_change = 1 if direction == RIGHT else -1
 
-        pos = (pos_x, pos_y)
-        if pos not in VISITED:
-            VISITED[pos] = 0
-        VISITED[pos] += 1
+        all_pos = (pos[ALL][0] + x_change, pos[ALL][1] + y_change)
+        turn_pos = (pos[turn][0] + x_change, pos[turn][1] + y_change)
 
-    # print VISITED
+        pos[ALL] = all_pos
+        pos[turn] = turn_pos
+
+        # print '%s - %s' % (ALL, all_pos)
+        # print '%s - %s' % (turn, turn_pos)
+
+        if all_pos not in PART_ONE_VISITED:
+            PART_ONE_VISITED[all_pos] = 0
+        PART_ONE_VISITED[all_pos] += 1
+
+        v = SANTA_VISITED if turn == SANTA else ROBOT_VISITED
+
+        if turn_pos not in v:
+            v[turn_pos] = 0
+        v[turn_pos] += 1
+        turn = ROBOT if turn == SANTA else SANTA
+
+    part_two_visited = set()
     houses_with_presents = 0
-    for k, v in VISITED.items():
+    for k, v in PART_ONE_VISITED.items():
         if v >= 1:
             houses_with_presents += 1
-    print houses_with_presents
+    for k, v in SANTA_VISITED.items():
+        if v >= 1:
+            part_two_visited.add(k)
+    for k, v in ROBOT_VISITED.items():
+        if v >= 1:
+            part_two_visited.add(k)
+    # print part_two_visited
+
+    print 'part 1 : %d' % houses_with_presents
+    print 'part 2 : %d' % len(part_two_visited)
 
 
 
