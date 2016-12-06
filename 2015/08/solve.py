8113e05b3cdee331248ebc4ec2aad1a3f39297d7
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2016-12-06 23:53:16 +0100

    2015 day 8 input

---
+++ 2015/08/solve.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+def main(lines):
+    print lines
+
+    part1 = ''
+    part2 = ''
+
+    print 'part 1 : %s' % (part1)
+    print 'part 2 : %s' % (part2)
+
+
+if __name__ == '__main__':
+    test_input = [
+        'test',
+        'strings',
+        'here'
+    ]
+    lines = []
+    if test_input:
+        lines = test_input
+    else:
+        with open('input') as file:
+            for line in file:
+                lines.append(line.strip())
+    main(lines)
