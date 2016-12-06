f3184de34c512882188ce25f17113055b02af61c
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2016-12-06 14:40:35 +0100

    2016 day 2 part two solution

--- 2016/02/solve.py
+++ 2016/02/solve.py
@@ -11,35 +11,50 @@ KEYPAD = [
     [7, 8, 9]
 ]
 
-def main(lines):
+KEYPAD_PART_TWO = [
+    [ '.', '.', '1', '.', '.' ],
+    [ '.', '2', '3', '4', '.' ],
+    [ '5', '6', '7', '8', '9' ],
+    [ '.', 'A', 'B', 'C', '.' ],
+    [ '.', '.', 'D', '.', '.' ],
+]
+
+
+def main(lines, keypad, start_row, start_col):
     # print lines
-    row = 1
-    col = 1
-    last_position = KEYPAD[row][col]
+    row = start_row
+    col = start_col
+    new_row = start_row
+    new_col = start_col
+    last_position = keypad[row][col]
     code = []
     for line in lines:
         for direction in line:
             if direction in [UP, DOWN]:
-                row = get_row(row, direction)
+                new_row = get_row(row, direction, keypad)
+                if not keypad[new_row][col] == '.':
+                    row = new_row
             if direction in [LEFT, RIGHT]:
-                col = get_col(col, direction)
-        last_position = KEYPAD[row][col]
+                new_col = get_col(col, direction, keypad)
+                if not keypad[row][new_col] == '.':
+                    col = new_col
+        last_position = keypad[row][col]
         code.append(last_position)
+
     print ''.join([str(x) for x in code])
 
-
-def get_row(current, direction):
+def get_row(current, direction, keypad):
     if direction == UP and current > 0:
         return current - 1
-    if direction == DOWN and current < 2:
+    if direction == DOWN and current < len(keypad) - 1:
         return current + 1
     return current
 
 
-def get_col(current, direction):
+def get_col(current, direction, keypad):
     if direction == LEFT and current > 0:
         return current - 1
-    if direction == RIGHT and current < 2:
+    if direction == RIGHT and current < len(keypad) - 1:
         return current + 1
     return current
 
@@ -49,4 +64,5 @@ if __name__ == '__main__':
     with open('input') as file:
         for line in file:
             lines.append(line.strip())
-    main(lines)
+    # main(lines, KEYPAD, 1, 1)
+    main(lines, KEYPAD_PART_TWO, 2, 0)
