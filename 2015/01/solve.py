68b23391423f5d667bb769fdea40be26ac3c7787
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2016-12-06 18:42:16 +0100

    2015 solve day 1 part 2

--- 2015/01/solve.py
+++ 2015/01/solve.py
@@ -6,12 +6,19 @@ RIGHT_PAREN = ')'
 def main(lines):
     parens = lines[0]
     floor = 0
+    position = 0
+    basement_found = False
     for paren in parens:
-        if paren == LEFT_PAREN:
-            floor += 1
-        elif paren == RIGHT_PAREN:
-            floor -= 1
-    print floor
+        updown = 1 if paren == LEFT_PAREN else -1
+
+        floor += updown
+        if not basement_found:
+            position += 1
+            if floor == -1:
+                basement_found = True
+
+    print 'part 1 : %d' % (floor)
+    print 'part 2 : %d' % (position)
 
 
 if __name__ == '__main__':
