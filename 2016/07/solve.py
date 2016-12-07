355b0ef5e1d9d7463ec23e975f572e1ec5e46c6c
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2016-12-07 15:42:52 +0100

    2016 day 7 part 2 solution

--- 2016/07/solve.py
+++ 2016/07/solve.py
@@ -20,6 +20,7 @@ def main(lines):
                 else:
                     supernet += line[i]
         part1 += part_one(line, supernet, hypernet)
+        part2 += part_two(line, supernet, hypernet)
     print 'part 1 : %d' % (part1)
     print 'part 2 : %d' % (part2)
 
@@ -40,6 +41,35 @@ def part_one(line, supernet, hypernet):
         return 1
     return 0
 
+def get_sequences_from_net(net):
+    sequences = set()
+
+    for i in range(0, len(net) - 2):
+        aba = net[i:i+3]
+        if len(aba) < 3:
+            continue
+        if aba == len(aba) * aba[0]:
+            continue
+        if aba[0] != aba[2] or aba[1] == aba[0]:
+            continue
+        sequences.add(aba)
+    return sequences
+
+def part_two(line, supernet, hypernet):
+    abas = get_sequences_from_net(supernet)
+    babs = get_sequences_from_net(hypernet)
+
+    if not abas or not babs:
+        return 0
+
+    for bab in babs:
+        aba = bab[1] + bab[0] + bab[1]
+        if aba in abas:
+            return 1
+
+    return 0
+
+
 if __name__ == '__main__':
     test_input = [
         # 'abba[mnop]qrst', #yes
@@ -47,6 +77,10 @@ if __name__ == '__main__':
         # 'aaaa[qwer]tyui', #no
         # 'ioxxoj[asdfgh]zxcvbn', #yes
         # 'i[oxxo]j[asdfgh]zxcvbn', #yes
+        # 'aba[bab]xyz', # yes
+        # 'xyx[xyx]xyx', # no
+        # 'aaa[kek]eke', # yes
+        # 'zazbz[bzb]cdb', # yes
     ]
     lines = []
     if test_input:
