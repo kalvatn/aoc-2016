b9ad99d7d8fd80c8a03b2fbf30a2d064c2bf5422
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2016-12-07 22:39:53 +0100

    2015 day 10 part 1 solution

--- 2015/10/solve.py
+++ 2015/10/solve.py
@@ -1,21 +1,46 @@
 #!/usr/bin/env python
 
+ITERATIONS = 40
 def main(lines):
-    sequence = lines[0]
+    for sequence in lines:
+        print 'old : ' + sequence
+        for i in range(1, ITERATIONS+1):
+            sequence = new_sequence(sequence)
+
+        part1 = len(sequence)
+        part2 = ''
+
+        print 'part 1 : %s' % (part1)
+        print 'part 2 : %s' % (part2)
+
+def new_sequence(sequence):
+    digit = sequence[0]
+    count = 0
+    builder = []
+    for i in sequence:
+        if i != digit:
+            builder.append(str(count) + digit)
+            digit = i
+            count = 0
+        count += 1
+
+    builder.append(str(count) + digit)
+    return ''.join(builder)
 
 
-    part1 = len(sequence)
-    part2 = ''
 
-    print 'part 1 : %s' % (part1)
-    print 'part 2 : %s' % (part2)
+
+
 
 
 if __name__ == '__main__':
     test_input = [
-        # 'test',
-        # 'strings',
-        # 'here'
+        # '1',
+        # '11',
+        # '21',
+        # '1211',
+        # '111221'
+
     ]
     lines = []
     if test_input:
