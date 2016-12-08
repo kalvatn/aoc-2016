3ce90d4603dd4bba58285fa3954a5564f47d588b
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2016-12-08 13:11:50 +0100

    2016 day 8 part 1 solution

--- 2016/08/solve.py
+++ 2016/08/solve.py
@@ -1,20 +1,90 @@
 #!/usr/bin/env python
 
-def main(lines):
-    print lines
+from collections import deque
 
-    part1 = ''
+
+ON  = 1
+OFF = 0
+# WIDTH  = 7
+# HEIGHT = 3
+
+WIDTH  = 50
+HEIGHT = 6
+SCREEN = [deque([ OFF for x in range(WIDTH)]) for y in range(HEIGHT)]
+
+
+CREATE_RECT = 'rect'
+ROTATE_ROW  = 'rotate row y='
+ROTATE_COL  = 'rotate column x='
+
+def main(lines):
+    print_screen()
+
+    for line in lines:
+        # print line
+        if line.startswith(CREATE_RECT):
+            width, height = [ int(x) for x in line.split(CREATE_RECT)[1].split('x') ]
+            create_rect(width, height)
+        elif line.startswith(ROTATE_ROW):
+            row, by = [ int(x) for x in line.split(ROTATE_ROW)[1].split('by') ]
+            rotate_row(row, by)
+        elif line.startswith(ROTATE_COL):
+            col, by = [ int(x) for x in line.split(ROTATE_COL)[1].split('by') ]
+            rotate_col(col, by)
+        # print_screen()
+
+
+
+    print_screen()
+
+
+    part1 = count_on()
     part2 = ''
 
     print 'part 1 : %s' % (part1)
     print 'part 2 : %s' % (part2)
 
 
+
+def create_rect(width, height):
+    print '%s %dx%d' % (CREATE_RECT, width, height)
+    for y in range(0, width):
+        for x in range(0, height):
+            SCREEN[x][y] = ON
+
+def rotate_row(row, by):
+    print '%s %d by %d' % (ROTATE_ROW, row, by)
+    SCREEN[row].rotate(by)
+
+def rotate_col(col, by):
+    print '%s %d by %d' % (ROTATE_COL, col, by)
+    items = deque([ SCREEN[y][col] for y in range(0, HEIGHT)])
+    for y in range(0, HEIGHT):
+        items.append(SCREEN[y][col])
+    items.rotate(by)
+    for y in range(0, HEIGHT):
+        SCREEN[y][col] = items[y]
+
+
+def print_screen():
+    for row in SCREEN:
+        print ' '.join([ str(x) for x in row])
+
+def count_on():
+    count = 0
+    for row in SCREEN:
+        count += sum([ x for x in row])
+    return count
+
+
+
+
 if __name__ == '__main__':
     test_input = [
-        'test',
-        'strings',
-        'here'
+        # 'rect 3x2',
+        # 'rotate column x=1 by 1',
+        # 'rotate row y=0 by 4',
+        # 'rotate column x=1 by 1'
     ]
     lines = []
     if test_input:
