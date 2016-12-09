1643cfe699623b052553ef14e40733cb81f0167e
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2016-12-09 21:51:06 +0100

    2015 day 11 part 1 input and tests

---
+++ 2015/11/solve.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import unittest
+
+class Test(unittest.TestCase):
+    def test_part_one_examples(self):
+        cases = [
+            { 'input' : 'hijklmmn', 'expect' : False },
+            { 'input' : 'abbceffg', 'expect' : False },
+            { 'input' : 'abbcegjk', 'expect' : False }
+        ]
+        for case in cases:
+            case_input = case['input']
+            case_expect = case['expect']
+            actual = part_one(case_input)
+            self.assertEquals(actual, case_expect, "part_one('%s'), expected '%s', was '%s'" % (case_input, case_expect, actual))
+
+    def test_part_two_examples(self):
+        cases = [
+            { 'input' : '', 'expect' : '' },
+            { 'input' : '', 'expect' : '' },
+            { 'input' : '', 'expect' : '' }
+        ]
+
+        for case in cases:
+            case_input = case['input']
+            case_expect = case['expect']
+            actual = part_two(case_input)
+            self.assertEquals(actual, case_expect, "part_two('%s'), expected '%s', was '%s'" % (case_input, case_expect, actual))
+
+def main(lines):
+    print lines
+
+    puzzle_input = parse_lines(lines)
+
+    part1 = part_one(puzzle_input)
+    part2 = part_two(puzzle_input)
+
+    print 'part 1 : %s' % (part1)
+    print 'part 2 : %s' % (part2)
+
+def part_one(puzzle_input):
+    return True
+
+def part_two(puzzle_input):
+    return ''
+
+
+if __name__ == '__main__':
+    unittest.main()
+    lines = []
+    with open('input') as file:
+        for line in file:
+            lines.append(line.strip())
+    main(lines)
