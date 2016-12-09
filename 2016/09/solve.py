7f838ecf9c2a0dbd3f5f30f5f4fb509fd5673331
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2016-12-09 21:12:57 +0100

    2016 day 9 part 2 test cases

--- 2016/09/solve.py
+++ 2016/09/solve.py
@@ -17,6 +17,15 @@ class Test(unittest.TestCase):
             decompressed = decompress_one(case['input'])
             self.assertEqual(decompressed, case['expect'])
             self.assertEqual(len(decompressed), case['expect_length'])
+    def test_part_two(self):
+        cases = {
+            { 'input' : '(3x3)XYZ',                                                 'expect_length' : 9 },
+            { 'input' : 'X(8x2)(3x3)ABCY',                                          'expect_length' : 20 },
+            { 'input' : '(27x12)(20x12)(13x14)(7x10)(1x12)A',                       'expect_length' : 241920 },
+            { 'input' : '(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN', 'expect_length' : 445 },
+        }
+        for case in cases:
+            self.assertEqual(decompress_two(case['input']), case['expect_length'])
 
 LB = '('
 RB = ')'
@@ -49,6 +58,27 @@ def decompress_one(line):
             decompressed += line[i]
             i += 1
     return decompressed
+
+def decompress_two(line):
+    i = 0
+    length = 0
+    decompressed = ''
+    while i < len(line):
+        if line[i] == LB:
+            i += 1
+            close = line.find(RB, i)
+            repeat_range, repeat = [int (c) for c in line[i:close].split('x')]
+            i += close - i + 1
+            repeat_text = line[i:i+repeat_range]
+            if repeat_text.find(LB):
+                length += decompress_two(repeat_text)
+
+            decompressed += repeat_text * repeat
+            i += repeat_range
+        else:
+            decompressed += line[i]
+            i += 1
+    return len(decompressed)
 
 
 if __name__ == '__main__':
