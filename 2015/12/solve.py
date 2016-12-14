bfd25ae793c03b182eb358b44f22c0ef3f9112f4
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2016-12-14 19:01:28 +0100

    2015 day 12 part 1 solution

--- 2015/12/solve.py
+++ 2015/12/solve.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import unittest
+import json
 
 class Test(unittest.TestCase):
     def test_part_one_examples(self):
@@ -9,15 +10,35 @@ class Test(unittest.TestCase):
     def test_part_two_examples(self):
         pass
 
+def get_number_sum(data):
+    number_sum = 0
+    try:
+        number_sum += int(data)
+    except :
+        if isinstance(data, list):
+            for item in data:
+                # items.append(get_number_sum(item))
+                number_sum += get_number_sum(item)
+        elif isinstance(data, dict):
+            for k,v in data.items():
+                number_sum += get_number_sum(data[k])
+        else:
+            # print 'unknown data %s' % data
+            pass
+    return number_sum
+
 def main(lines):
-    part1 = None
+    json_data = json.loads(''.join(lines))
+
+    part1 = get_number_sum(json_data)
+
     part2 = None
 
     print 'part 1 : %s' % (part1)
     print 'part 2 : %s' % (part2)
 
 if __name__ == '__main__':
-    unittest.main()
+    # unittest.main()
     lines = []
     with open('input') as file:
         for line in file:
