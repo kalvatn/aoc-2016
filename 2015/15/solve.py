e93de62872dc8da2e058c99f671fcae20d2eeb5f
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2016-12-15 00:29:13 +0100

    2015 day 15 example solution

--- 2015/15/solve.py
+++ 2015/15/solve.py
@@ -3,6 +3,9 @@
 import unittest
 import re
 
+from itertools import permutations
+from operator import mul
+
 class Test(unittest.TestCase):
     def test_part_one_examples(self):
         pass
@@ -20,10 +23,46 @@ class Ingredient(object):
         self.calories = calories
 
     def __str__(self):
-        return '%10s (CAP:%2d DUR:%2d FLA:%2d TEX:%2d CAL:%2d)' % (self.name, self.capacity, self.durability, self.flavor, self.texture, self.calories)
+        return '%15s (CAP:%2d DUR:%2d FLA:%2d TEX:%2d CAL:%2d)' % (self.name, self.capacity, self.durability, self.flavor, self.texture, self.calories)
 
     def __repr__(self):
         return '%s' % self.name
+
+def get_integer_combinations(number_of_variables, max_range):
+    numbers = range(0, max_range + 1)
+    return [ combination for combination in permutations(numbers, number_of_variables) if sum(combination) == max_range ]
+
+
+def zero_if_negative(value):
+    return value if value > 0 else 0
+
+def get_optimal_spoon_distribution(ingredients, max_spoons):
+    best = None
+    number_of_variables = len(ingredients)
+    for combination in get_integer_combinations(number_of_variables, max_spoons):
+        properties = {
+            'capacity' : 0,
+            'durability' : 0,
+            'flavor' : 0,
+            'texture' : 0
+        }
+        for i in range(0, number_of_variables):
+            ingredient = ingredients[i]
+            spoons = combination[i]
+            properties['capacity'] += (ingredient.capacity * spoons)
+            properties['durability'] += (ingredient.durability * spoons)
+            properties['flavor'] += (ingredient.flavor * spoons)
+            properties['texture'] += (ingredient.texture * spoons)
+
+        factors = [ zero_if_negative(v) for v in properties.values() ]
+        product = reduce(mul, factors, 1)
+        if best is None or product > best[0]:
+            best = (product, combination)
+    return best
+
+
+
+
 
 def main(lines):
     part1 = None
@@ -38,8 +77,10 @@ def main(lines):
     for ingredient in ingredients:
         print ingredient
 
+    part1 = get_optimal_spoon_distribution(ingredients, 100)
 
-    print 'part 1 : %s' % (part1)
+
+    print 'part 1 : %d (%s)' % (part1[0], part1[1])
     print 'part 2 : %s' % (part2)
 
 if __name__ == '__main__':
