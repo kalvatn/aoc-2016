93494bea92424001ea90b31eabf7e6e4878173bc
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2016-12-14 14:12:38 +0100

    2016 day 14 part 1 solution

--- 2016/14/solve.py
+++ 2016/14/solve.py
@@ -17,15 +17,15 @@ def main(lines):
     numbers = ''.join([str(i) for i in range(0, 10)])
     triplets = [ a*3 for a in numbers + letters ]
     quintuplets = [ a*5 for a in numbers + letters ]
-    # salt = lines[0]
-    salt = 'abc'
+    salt = lines[0]
+    # salt = 'abc'
     candidates = {}
 
     triplet_hashes = {}
     quintuplet_hashes = {}
     found_key_indexes = set()
     i = 0
-    while len(found_key_indexes) < 65:
+    while len(found_key_indexes) < 64:
         tohash = '%s%d' % (salt, i)
         hashed = hashlib.md5(tohash).hexdigest()
         for triplet in triplets:
@@ -38,24 +38,24 @@ def main(lines):
         i += 1
         if i % 10000 == 0:
             for tk, tv in triplet_hashes.items():
-                for qk, qv in quintuplet_hashes.items():
-                    if qv[1].startswith(tv) and qk > tk and (qk - tk <= 1000):
-                        found_key_indexes.add((tk, qk))
-                        # print '%d -> %d (%s -> %s)' % (tk, qk, triplet_hashes[tk], quintuplet_hashes[qk])
+                if tk not in found_key_indexes:
+                    for qk, qv in quintuplet_hashes.items():
+                        if qv[1].startswith(tv) and qk > tk and (qk - tk <= 1000):
+                            if tk not in found_key_indexes:
+                                found_key_indexes.add((tk, qk))
+                            # print '%d -> %d (%s -> %s)' % (tk, qk, triplet_hashes[tk], quintuplet_hashes[qk])
 
     print salt
+    # found_key_indexes = sorted(found_key_indexes)
     found_key_indexes = sorted(found_key_indexes)
+    print found_key_indexes[63]
     i = 0
     for tk, qk in found_key_indexes:
         triplet = triplet_hashes[tk]
         quintuplet = quintuplet_hashes[qk]
 
+        print '%d -> %d (%d) (%s -> %s)' % (tk, qk, qk - tk, triplet, quintuplet)
 
-        i += 1
-        if i == 64:
-            break
-
-        print '%d -> %d (%s -> %s)' % (tk, qk, triplet, quintuplet)
 
 
 
