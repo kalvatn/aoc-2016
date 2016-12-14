38e07db219887fcb42ba374aada9ca9cebb14bd3
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2016-12-14 13:40:14 +0100

    2016 day 14 example solution

--- 2016/14/solve.py
+++ 2016/14/solve.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import unittest
+import hashlib
+from string import ascii_lowercase as letters
 
 class Test(unittest.TestCase):
     def test_part_one_examples(self):
@@ -9,9 +11,55 @@ class Test(unittest.TestCase):
     def test_part_two_examples(self):
         pass
 
+
+
 def main(lines):
-    salt = lines[0]
+    numbers = ''.join([str(i) for i in range(0, 10)])
+    triplets = [ a*3 for a in numbers + letters ]
+    quintuplets = [ a*5 for a in numbers + letters ]
+    # salt = lines[0]
+    salt = 'abc'
+    candidates = {}
+
+    triplet_hashes = {}
+    quintuplet_hashes = {}
+    found_key_indexes = set()
+    i = 0
+    while len(found_key_indexes) < 65:
+        tohash = '%s%d' % (salt, i)
+        hashed = hashlib.md5(tohash).hexdigest()
+        for triplet in triplets:
+            if triplet in hashed:
+                triplet_hashes[i] = (hashed, triplet)
+        for quintuplet in quintuplets:
+            if quintuplet in hashed:
+                quintuplet_hashes[i] = (hashed, quintuplet)
+        # print '%d, tohash : %s, hash : %s' % (i, tohash, hashed)
+        i += 1
+        if i % 10000 == 0:
+            for tk, tv in triplet_hashes.items():
+                for qk, qv in quintuplet_hashes.items():
+                    if qv[1].startswith(tv) and qk > tk and (qk - tk <= 1000):
+                        found_key_indexes.add((tk, qk))
+                        # print '%d -> %d (%s -> %s)' % (tk, qk, triplet_hashes[tk], quintuplet_hashes[qk])
+
     print salt
+    found_key_indexes = sorted(found_key_indexes)
+    i = 0
+    for tk, qk in found_key_indexes:
+        triplet = triplet_hashes[tk]
+        quintuplet = quintuplet_hashes[qk]
+
+
+        i += 1
+        if i == 64:
+            break
+
+        print '%d -> %d (%s -> %s)' % (tk, qk, triplet, quintuplet)
+
+
+
+
     part1 = None
     part2 = None
 
