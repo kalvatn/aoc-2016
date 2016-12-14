604fa1e536c003cb2d7b91d2b583b6882ca9eea3
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2016-12-14 20:55:27 +0100

    2015 day 14 example solution

--- 2015/14/solve.py
+++ 2015/14/solve.py
@@ -16,6 +16,23 @@ class Reindeer(object):
         self.speed = speed
         self.fly_duration = fly_duration
         self.rest_duration = rest_duration
+        self.total_distance = 0
+
+        self.rest_counter = 0
+        self.fly_counter = 0
+
+    def fly(self):
+        if self.rest_counter > 0:
+            self.rest_counter -= 1
+            if self.rest_counter == 0:
+                self.fly_counter = 0
+        else:
+            if self.fly_counter < self.fly_duration:
+                self.fly_counter += 1
+                self.total_distance += self.speed
+            else:
+                self.rest_counter = self.rest_duration
+        print '%s fly : %d, rest : %d, distance : %d' % (self.name, self.fly_counter, self.rest_counter, self.total_distance)
 
     def __str__(self):
         return '%s %dkm/s for %d, rest %d' % (self.name, self.speed, self.fly_duration, self.rest_duration)
@@ -31,8 +48,10 @@ def main(lines):
         name, speed, fly_duration, rest_duration = match.groups()
         reindeers.append(Reindeer(name, int(speed), int(fly_duration), int(rest_duration)))
 
-    for reindeer in reindeers:
-        print reindeer
+    print reindeers
+    for i in range(0, 1000):
+        for reindeer in reindeers:
+            reindeer.fly()
 
     print 'part 1 : %s' % (part1)
     print 'part 2 : %s' % (part2)
@@ -40,7 +59,8 @@ def main(lines):
 if __name__ == '__main__':
     # unittest.main()
     lines = []
-    with open('input') as file:
+    # with open('input') as file:
+    with open('example_input') as file:
         for line in file:
             lines.append(line.strip())
     main(lines)
