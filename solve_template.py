091b207d9f5ed476178088b661f5bb96d6512994
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2016-12-14 12:55:52 +0100

    modify solve template

--- solve_template.py
+++ solve_template.py
@@ -4,47 +4,17 @@ import unittest
 
 class Test(unittest.TestCase):
     def test_part_one_examples(self):
-        cases = [
-            { 'input' : '', 'expect' : '' },
-            { 'input' : '', 'expect' : '' },
-            { 'input' : '', 'expect' : '' }
-        ]
-        for case in cases:
-            case_input = case['input']
-            case_expect = case['expect']
-            actual = part_one(case_input)
-            self.assertEquals(actual, case_expect, "part_one('%s'), expected '%s', was '%s'" % (case_input, case_expect, actual))
+        pass
 
     def test_part_two_examples(self):
-        cases = [
-            { 'input' : '', 'expect' : '' },
-            { 'input' : '', 'expect' : '' },
-            { 'input' : '', 'expect' : '' }
-        ]
-
-        for case in cases:
-            case_input = case['input']
-            case_expect = case['expect']
-            actual = part_two(case_input)
-            self.assertEquals(actual, case_expect, "part_two('%s'), expected '%s', was '%s'" % (case_input, case_expect, actual))
+        pass
 
 def main(lines):
-    print lines
-
-    puzzle_input = parse_lines(lines)
-
-    part1 = part_one(puzzle_input)
-    part2 = part_two(puzzle_input)
+    part1 = None
+    part2 = None
 
     print 'part 1 : %s' % (part1)
     print 'part 2 : %s' % (part2)
-
-def part_one(puzzle_input):
-    return ''
-
-def part_two(puzzle_input):
-    return ''
-
 
 if __name__ == '__main__':
     unittest.main()
