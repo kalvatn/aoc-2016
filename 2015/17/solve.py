13d0c6e674e1749f260ba9cb5a7ec109c63868bc
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2016-12-15 02:34:30 +0100

    2015 day 17 example solution

--- 2015/17/solve.py
+++ 2015/17/solve.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 import unittest
+from itertools import permutations
+from itertools import combinations_with_replacement
+from itertools import combinations
 
 class Test(unittest.TestCase):
     def test_part_one_examples(self):
@@ -9,15 +12,30 @@ class Test(unittest.TestCase):
     def test_part_two_examples(self):
         pass
 
+def get_combinations(container_sizes, liters):
+    total = []
+    for i in range(2, 10):
+        combos = [ combination for combination in combinations(container_sizes, i) if sum(combination) == liters ]
+        for combo in combos:
+            print combo
+            total.append(combo)
+    return total
+
+
+
 def main(lines):
-    part1 = None
+    container_sizes = [ 20, 15, 10, 5, 5 ]
+    part1 = len(get_combinations(container_sizes, 25))
+
+    # container_sizes = [ int(line) for line in lines ]
+    # part1 = len(get_combinations(container_sizes, 150))
     part2 = None
 
     print 'part 1 : %s' % (part1)
     print 'part 2 : %s' % (part2)
 
 if __name__ == '__main__':
-    unittest.main()
+    # unittest.main()
     lines = []
     with open('input') as file:
         for line in file:
