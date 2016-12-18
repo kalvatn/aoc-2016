a2854c66a95f14a70ebeca109eecd2cddf755223
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2016-12-18 22:49:13 +0100

    module-ify 2015 day 17

--- 2015/17/solve.py
+++ 2015/17/solve.py
@@ -1,57 +1,46 @@
 #!/usr/bin/env python
 
-import unittest
 from itertools import permutations
 from itertools import combinations_with_replacement
 from itertools import combinations
 
-class Test(unittest.TestCase):
-    def test_part_one_examples(self):
-        pass
+from stdlib import aoc
 
-    def test_part_two_examples(self):
-        pass
+class Day17(aoc.Day):
+    def __init__(self):
+        super(Day17, self).__init__(__file__)
 
-def get_combinations(container_sizes, liters, minimum_combo_size=False):
-    total = []
-    i = 1
-    minimum_combo = None
-    while True:
-        combos = [ combination for combination in combinations(container_sizes, i) if sum(combination) == liters ]
-        if not combos and i > 10:
-            break
-        for combo in combos:
-            if minimum_combo is None or len(combo) < minimum_combo:
-                minimum_combo = len(combo)
-            total.append(combo)
-        i += 1
-    if minimum_combo_size:
-        minimum_total = []
-        for combo in total:
-            if len(combo) == minimum_combo:
-                minimum_total.append(combo)
-        return minimum_total
-    else:
-        return total
+    def get_combinations(self, container_sizes, liters, minimum_combo_size=False):
+        total = []
+        i = 1
+        minimum_combo = None
+        while True:
+            combos = [ combination for combination in combinations(container_sizes, i) if sum(combination) == liters ]
+            if not combos and i > 10:
+                break
+            for combo in combos:
+                if minimum_combo is None or len(combo) < minimum_combo:
+                    minimum_combo = len(combo)
+                total.append(combo)
+            i += 1
+        if minimum_combo_size:
+            minimum_total = []
+            for combo in total:
+                if len(combo) == minimum_combo:
+                    minimum_total.append(combo)
+            return minimum_total
+        else:
+            return total
 
+    def run(self):
+        lines = self.read_input()
 
+        container_sizes = [ int(line) for line in lines ]
+        part1 = len(self.get_combinations(container_sizes, 150))
+        part2 = len(self.get_combinations(container_sizes, 150, minimum_combo_size=True))
 
-def main(lines):
-    # container_sizes = [ 20, 15, 10, 5, 5 ]
-    # part1 = len(get_combinations(container_sizes, 25))
-    # part2 = len(get_combinations(container_sizes, 25, minimum_combo_size=True))
-
-    container_sizes = [ int(line) for line in lines ]
-    part1 = len(get_combinations(container_sizes, 150))
-    part2 = len(get_combinations(container_sizes, 150, minimum_combo_size=True))
-
-    print 'part 1 : %s' % (part1)
-    print 'part 2 : %s' % (part2)
+        self.log.info('part 1 : %s' % (part1))
+        self.log.info('part 2 : %s' % (part2))
 
 if __name__ == '__main__':
-    # unittest.main()
-    lines = []
-    with open('input') as file:
-        for line in file:
-            lines.append(line.strip())
-    main(lines)
+    Day17().run()
