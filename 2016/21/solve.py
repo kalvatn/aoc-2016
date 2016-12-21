2331551eeeb38817876aaae3a94e9f73305555a9
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2016-12-21 16:55:01 +0100

    2016 day 21 part 1 solution

--- 2016/21/solve.py
+++ 2016/21/solve.py
@@ -1,31 +1,59 @@
 from stdlib import aoc
-"""
-The scrambling function is a series of operations (the exact list is provided in your puzzle input). Starting with the password to be scrambled, apply each operation in succession to the string. The individual operations behave as follows:
-
-    swap position X with position Y means that the letters at indexes X and Y (counting from 0) should be swapped.
-    swap letter X with letter Y means that the letters X and Y should be swapped (regardless of where they appear in the string).
-    rotate left/right X steps means that the whole string should be rotated; for example, one right rotation would turn abcd into dabc.
-    rotate based on position of letter X means that the whole string should be rotated to the right based on the index of letter X (counting from 0) as determined before this instruction does any rotations. Once the index is determined, rotate the string to the right one time, plus a number of times equal to that index, plus one additional time if the index was at least 4.
-    reverse positions X through Y means that the span of letters at indexes X through Y (including the letters at X and Y) should be reversed in order.
-    move position X to position Y means that the letter which is at index X should be removed from the string, then inserted such that it ends up at index Y.
-
-For example, suppose you start with abcde and perform the following operations:
-
-    swap position 4 with position 0 swaps the first and last letters, producing the input for the next step, ebcda.
-    swap letter d with letter b swaps the positions of d and b: edcba.
-    reverse positions 0 through 4 causes the entire string to be reversed, producing abcde.
-    rotate left 1 step shifts all letters left one position, causing the first letter to wrap to the end of the string: bcdea.
-    move position 1 to position 4 removes the letter at position 1 (c), then inserts it at position 4 (the end of the string): bdeac.
-    move position 3 to position 0 removes the letter at position 3 (a), then inserts it at position 0 (the front of the string): abdec.
-    rotate based on position of letter b finds the index of letter b (1), then rotates the string right once plus a number of times equal to that index (2): ecabd.
-    rotate based on position of letter d finds the index of letter d (4), then rotates the string right once, plus a number of times equal to that index, plus an additional time because the index was at least 4, for a total of 6 right rotations: decab.
-"""
+from stdlib import stringutils
+import re
 
 class Day21(aoc.Day):
     def __init__(self):
         super(Day21, self).__init__(__file__)
 
     def run(self):
+        swap_pos = re.compile(r'swap position (\d) with position (\d)')
+        swap_letter = re.compile(r'swap letter (\w) with letter (\w)')
+        rotate_by_steps = re.compile(r'rotate (left|right) (\d) steps?')
+        rotate_by_letter = re.compile(r'rotate based on position of letter (\w)')
+        reverse_range = re.compile(r'reverse positions (\d) through (\d)')
+        move_letter = re.compile(r'move position (\d) to position (\d)')
+
+        s = 'abcdefgh'
+        for line in self.read_input():
+            before = s
+            if swap_pos.match(line):
+                from_index, to_index = swap_pos.match(line).groups()
+                self.log.debug('%s -> SWAP pos %d and %d' % (line, int(from_index), int(to_index)))
+                s = stringutils.swap_index(s, int(from_index), int(to_index))
+            elif swap_letter.match(line):
+                x, y = swap_letter.match(line).groups()
+                self.log.debug('%s -> SWAP letter %s and %s' % (line, x, y))
+                s = stringutils.swap_char(s, x, y)
+            elif rotate_by_steps.match(line):
+                leftright, x = rotate_by_steps.match(line).groups()
+                x = int(x)
+                if leftright == 'left':
+                    x *= -1
+                self.log.debug('%s -> ROTATE %s %d times' % (line, leftright, int(x)))
+                s = stringutils.rotate(s, x)
+            elif rotate_by_letter.match(line):
+                x = rotate_by_letter.match(line).groups()[0]
+                self.log.debug('%s -> ROTATE by letter %s' % (line, x))
+                s = stringutils.rotate_by_char_pos_day21(s, x)
+            elif reverse_range.match(line):
+                x, y = reverse_range.match(line).groups()
+                self.log.debug('%s -> REVERSE range %d to %d' % (line, int(x), int(y)))
+                s = stringutils.reverse_range(s, int(x), int(y))
+            elif move_letter.match(line):
+                x, y = move_letter.match(line).groups()
+                self.log.debug('%s -> MOVE letter at %d to %d' % (line, int(x), int(y)))
+                s = stringutils.move_char(s, int(x), int(y))
+            else:
+                self.log.error('%s not matched' % line)
+            if before == s:
+                self.log.warn('%s no change' % line)
+            self.log.debug('%s -> %s' % (before, s))
+
+
+
+
+
         part1, part2 = None, None
         self.log.info('part 1 : %s' % (part1))
         self.log.info('part 2 : %s' % (part2))
