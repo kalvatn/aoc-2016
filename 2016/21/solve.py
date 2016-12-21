1ca23798b6023c5ccb8bd3e76df2af8539409f52
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2016-12-21 21:39:00 +0100

    2016 day 21 reverse part 1

--- 2016/21/solve.py
+++ 2016/21/solve.py
@@ -6,7 +6,7 @@ class Day21(aoc.Day):
     def __init__(self):
         super(Day21, self).__init__(__file__)
 
-    def scramble(self, password):
+    def scramble(self, password, unscramble=False):
 
         swap_pos = re.compile(r'swap position (\d) with position (\d)')
         swap_letter = re.compile(r'swap letter (\w) with letter (\w)')
@@ -15,48 +15,72 @@ class Day21(aoc.Day):
         reverse_range = re.compile(r'reverse positions (\d) through (\d)')
         move_letter = re.compile(r'move position (\d) to position (\d)')
 
-        for line in self.read_input():
+        lines = self.read_input()
+        if unscramble:
+            lines = reversed(lines)
+        for line in lines:
             before = password
             if swap_pos.match(line):
                 from_index, to_index = swap_pos.match(line).groups()
-                self.log.debug('%s -> SWAP pos %d and %d' % (line, int(from_index), int(to_index)))
+                # self.log.debug('%s -> SWAP pos %d and %d' % (line, int(from_index), int(to_index)))
+                if unscramble:
+                    from_index, to_index = to_index, from_index
                 password = stringutils.swap_index(password, int(from_index), int(to_index))
             elif swap_letter.match(line):
                 x, y = swap_letter.match(line).groups()
-                self.log.debug('%s -> SWAP letter %s and %s' % (line, x, y))
+                # self.log.debug('%s -> SWAP letter %s and %s' % (line, x, y))
+                if unscramble:
+                    x, y = y, x
                 password = stringutils.swap_char(password, x, y)
             elif rotate_by_steps.match(line):
                 leftright, x = rotate_by_steps.match(line).groups()
                 x = int(x)
                 if leftright == 'left':
                     x *= -1
-                self.log.debug('%s -> ROTATE %s %d times' % (line, leftright, int(x)))
+                # self.log.debug('%s -> ROTATE %s %d times' % (line, leftright, int(x)))
+                if unscramble:
+                    x *= -1
                 password = stringutils.rotate(password, x)
             elif rotate_by_letter.match(line):
                 x = rotate_by_letter.match(line).groups()[0]
-                self.log.debug('%s -> ROTATE by letter %s' % (line, x))
-                password = stringutils.rotate_by_char_pos_day21(password, x)
+                # self.log.debug('%s -> ROTATE by letter %s' % (line, x))
+
+                if unscramble:
+                    index = password.index(x)
+                    original_positions_from_current_index = {0: 7, 1: 0, 2: 4, 3: 1, 4: 5, 5: 2, 6: 6, 7: 3}
+                    num_rotations = original_positions_from_current_index[index] - index
+                else:
+                    index = password.index(x)
+                    num_rotations = 1 + index
+                    if index >= 4:
+                        num_rotations += 1
+
+                password = stringutils.rotate(password, num_rotations)
             elif reverse_range.match(line):
                 x, y = reverse_range.match(line).groups()
-                self.log.debug('%s -> REVERSE range %d to %d' % (line, int(x), int(y)))
+                # self.log.debug('%s -> REVERSE range %d to %d' % (line, int(x), int(y)))
                 password = stringutils.reverse_range(password, int(x), int(y))
             elif move_letter.match(line):
                 x, y = move_letter.match(line).groups()
-                self.log.debug('%s -> MOVE letter at %d to %d' % (line, int(x), int(y)))
+                # self.log.debug('%s -> MOVE letter at %d to %d' % (line, int(x), int(y)))
+                if unscramble:
+                    x, y = y, x
                 password = stringutils.move_char(password, int(x), int(y))
             else:
-                self.log.error('%s not matched' % line)
+                raise ValueError('invalid instruction %s' % (line))
             if before == password:
                 self.log.warn('%s no change' % line)
-            self.log.debug('%s -> %s' % (before, password))
+            if unscramble:
+                self.log.debug('UNSCRAMBLE : %s %s -> %s' % (before, line,  password))
+            else:
+                self.log.debug('SCRAMBLE   : %s %s -> %s' % (before, line,  password))
         return password
 
 
     def run(self):
-
         password = 'abcdefgh'
         part1 = self.scramble(password)
-        part2 = None
+        part2 = self.scramble(part1, unscramble=True)
 
         self.log.info('part 1 : %s' % (part1))
         self.log.info('part 2 : %s' % (part2))
