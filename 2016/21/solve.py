17b2051813c2ea24603350a837aa03bf6aa24f1b
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2016-12-21 11:47:13 +0100

    2016 day 21 solve template

---
+++ 2016/21/solve.py
@@ -0,0 +1,13 @@
+from stdlib import aoc
+
+class Day21(aoc.Day):
+    def __init__(self):
+        super(Day21, self).__init__(__file__)
+
+    def run(self):
+        part1, part2 = None, None
+        self.log.info('part 1 : %s' % (part1))
+        self.log.info('part 2 : %s' % (part2))
+
+if __name__ == '__main__':
+    Day21().run()
