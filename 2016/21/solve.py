7983cdf99b06c9cec33a77aed3a4e5d05016d6b6
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2016-12-21 16:59:18 +0100

    2016 day 21 part 1 cleanup

--- 2016/21/solve.py
+++ 2016/21/solve.py
@@ -6,7 +6,8 @@ class Day21(aoc.Day):
     def __init__(self):
         super(Day21, self).__init__(__file__)
 
-    def run(self):
+    def scramble(self, password):
+
         swap_pos = re.compile(r'swap position (\d) with position (\d)')
         swap_letter = re.compile(r'swap letter (\w) with letter (\w)')
         rotate_by_steps = re.compile(r'rotate (left|right) (\d) steps?')
@@ -14,47 +15,49 @@ class Day21(aoc.Day):
         reverse_range = re.compile(r'reverse positions (\d) through (\d)')
         move_letter = re.compile(r'move position (\d) to position (\d)')
 
-        s = 'abcdefgh'
         for line in self.read_input():
-            before = s
+            before = password
             if swap_pos.match(line):
                 from_index, to_index = swap_pos.match(line).groups()
                 self.log.debug('%s -> SWAP pos %d and %d' % (line, int(from_index), int(to_index)))
-                s = stringutils.swap_index(s, int(from_index), int(to_index))
+                password = stringutils.swap_index(password, int(from_index), int(to_index))
             elif swap_letter.match(line):
                 x, y = swap_letter.match(line).groups()
                 self.log.debug('%s -> SWAP letter %s and %s' % (line, x, y))
-                s = stringutils.swap_char(s, x, y)
+                password = stringutils.swap_char(password, x, y)
             elif rotate_by_steps.match(line):
                 leftright, x = rotate_by_steps.match(line).groups()
                 x = int(x)
                 if leftright == 'left':
                     x *= -1
                 self.log.debug('%s -> ROTATE %s %d times' % (line, leftright, int(x)))
-                s = stringutils.rotate(s, x)
+                password = stringutils.rotate(password, x)
             elif rotate_by_letter.match(line):
                 x = rotate_by_letter.match(line).groups()[0]
                 self.log.debug('%s -> ROTATE by letter %s' % (line, x))
-                s = stringutils.rotate_by_char_pos_day21(s, x)
+                password = stringutils.rotate_by_char_pos_day21(password, x)
             elif reverse_range.match(line):
                 x, y = reverse_range.match(line).groups()
                 self.log.debug('%s -> REVERSE range %d to %d' % (line, int(x), int(y)))
-                s = stringutils.reverse_range(s, int(x), int(y))
+                password = stringutils.reverse_range(password, int(x), int(y))
             elif move_letter.match(line):
                 x, y = move_letter.match(line).groups()
                 self.log.debug('%s -> MOVE letter at %d to %d' % (line, int(x), int(y)))
-                s = stringutils.move_char(s, int(x), int(y))
+                password = stringutils.move_char(password, int(x), int(y))
             else:
                 self.log.error('%s not matched' % line)
-            if before == s:
+            if before == password:
                 self.log.warn('%s no change' % line)
-            self.log.debug('%s -> %s' % (before, s))
+            self.log.debug('%s -> %s' % (before, password))
+        return password
 
 
+    def run(self):
 
+        password = 'abcdefgh'
+        part1 = self.scramble(password)
+        part2 = None
 
-
-        part1, part2 = None, None
         self.log.info('part 1 : %s' % (part1))
         self.log.info('part 2 : %s' % (part2))
 
