8a63fc08310eac1e94c343f7f489e3266358a66f
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2016-12-23 08:42:34 +0100

    2016 day 23 description and input

---
+++ 2016/23/solve.py
@@ -0,0 +1,16 @@
+from stdlib import aoc
+
+class Day23(aoc.Day):
+    def __init__(self):
+        super(Day23, self).__init__(__file__)
+
+    def part_one(self):
+        return None
+
+    def run(self):
+        part1 = self.part_one()
+        self.log.info('part 1 : %s' % (part1))
+
+if __name__ == '__main__':
+    Day23().run()
+
